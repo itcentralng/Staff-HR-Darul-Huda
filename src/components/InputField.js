3873ae96bfbbd2c8a79e3fdc677b04f2d8c506a7
binumarcodes <abdulrahmanbinumar179@gmail.com>
2024-03-17 10:55:23 +0100

    Added Icons and Email update

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,27 +1,40 @@
 import React from 'react';
-import { TextInput, StyleSheet } from 'react-native';
+import { TextInput, StyleSheet, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons'; // Assuming you're using Expo
 
-const InputField = ({ placeholder, secureTextEntry, onChangeText }) => {
+const InputField = ({ placeholder, secureTextEntry, onChangeText, iconName }) => {
   return (
-    <TextInput
-      style={styles.input}
-      placeholder={placeholder}
-      secureTextEntry={secureTextEntry}
-      onChangeText={onChangeText}
-    />
+    <View style={styles.inputContainer}>
+      <FontAwesome name={iconName} style={styles.icon} />
+      <TextInput
+        style={styles.input}
+        placeholder={placeholder}
+        secureTextEntry={secureTextEntry}
+        onChangeText={onChangeText}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
     borderColor: 'gray',
     borderWidth: 1,
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 10,
+  },
+  input: {
+    flex: 1,
+    height: 40,
     fontFamily: 'Lato-Regular'
-
+  },
+  icon: {
+    marginRight: 10,
+    fontSize: 20,
+    color: 'gray',
   },
 });
 
