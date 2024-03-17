4fd6b825ec1049865b1bdc5aa9c39bc03d703c8a
binumarcodes <abdulrahmanbinumar179@gmail.com>
2024-03-17 06:50:22 +0100

    Added Background Image/UI

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import Header from '../components/Header';
 import InputField from '../components/InputField';
 import LoginButton from '../components/LoginButton';
@@ -13,13 +13,20 @@ const LoginScreen = ({ navigation }) => {
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    // Navigate to the 'Dashboard' tab
     navigation.navigate('Main', { screen: 'Dashboard' });
   };
   
 
   return (
     <View style={styles.container}>
+      <Image
+  source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPiLz6Rnxh_XPtDCSmZHRNY6mja4o7AMd7D11ugRxXONPr-WW9' }}
+  style={styles.imageTop}
+/>
+      <Image
+  source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPiLz6Rnxh_XPtDCSmZHRNY6mja4o7AMd7D11ugRxXONPr-WW9' }}
+  style={styles.imageBottom}
+/>
       <Header />
       <InputField
         placeholder="Username"
@@ -40,6 +47,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 20,
+    backgroundColor: "#fff"
+  },
+  imageTop: {
+    width: 200,
+    height: 200,
+    borderRadius: 8,
+    marginBottom: 20,
+    position: "absolute",
+    top: -30,
+    left: -30,
+    zIndex: 100
+  },
+  imageBottom: {
+    width: 200,
+    height: 200,
+    borderRadius: 8,
+    marginBottom: 20,
+    position: "absolute",
+    bottom: -50,
+    right: -50,
+    zIndex: 100
   },
 });
 
